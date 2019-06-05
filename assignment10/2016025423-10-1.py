bfd6f3d99c2faa865380db1400560645b60a46e6
hipiphock <hipiphock@naver.com>
2019-06-05 18:22:26 +0900

    deleted second render function

--- assignment10/2016025423-10-1.py
+++ assignment10/2016025423-10-1.py
@@ -329,75 +329,75 @@ if __name__ == "__main__":
 
 ###################################################
 # [Practice] Properties of Rotation Matrix
-def render():
-    global gCamAng, gCamHeight
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-    glEnable(GL_DEPTH_TEST)
+# def render():
+#     global gCamAng, gCamHeight
+#     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+#     glEnable(GL_DEPTH_TEST)
 
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(45, 1, 1,10)
+#     glMatrixMode(GL_PROJECTION)
+#     glLoadIdentity()
+#     gluPerspective(45, 1, 1,10)
 
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-    gluLookAt(5*np.sin(gCamAng),gCamHeight,5*np.cos(gCamAng), 0,0,0, 0,1,0)
+#     glMatrixMode(GL_MODELVIEW)
+#     glLoadIdentity()
+#     gluLookAt(5*np.sin(gCamAng),gCamHeight,5*np.cos(gCamAng), 0,0,0, 0,1,0)
 
-    drawFrame() # draw global frame
+#     drawFrame() # draw global frame
 
-    glEnable(GL_LIGHTING)
-    glEnable(GL_LIGHT0)
-    glEnable(GL_RESCALE_NORMAL) # rescale normal
+#     glEnable(GL_LIGHTING)
+#     glEnable(GL_LIGHT0)
+#     glEnable(GL_RESCALE_NORMAL) # rescale normal
 
-    glLightfv(GL_LIGHT0, GL_POSITION, (1.,2.,3.,1.))
-    glLightfv(GL_LIGHT0, GL_AMBIENT, (.1,.1,.1,1.))
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, (1.,1.,1.,1.))
-    glLightfv(GL_LIGHT0, GL_SPECULAR, (1.,1.,1.,1.))
+#     glLightfv(GL_LIGHT0, GL_POSITION, (1.,2.,3.,1.))
+#     glLightfv(GL_LIGHT0, GL_AMBIENT, (.1,.1,.1,1.))
+#     glLightfv(GL_LIGHT0, GL_DIFFUSE, (1.,1.,1.,1.))
+#     glLightfv(GL_LIGHT0, GL_SPECULAR, (1.,1.,1.,1.))
 
-    # ZYX Euler angles
-    t = glfw.get_time()
-    xang = t
-    yang = np.radians(30)
-    zang = np.radians(30)
-    M = np.identity(4)
-    Rx = np.array([[1,0,0],
-                   [0, np.cos(xang), -np.sin(xang)],
-                   [0, np.sin(xang), np.cos(xang)]])
-    Ry = np.array([[np.cos(yang), 0, np.sin(yang)],
-                   [0,1,0],
-                   [-np.sin(yang), 0, np.cos(yang)]])
-    Rz = np.array([[np.cos(zang), -np.sin(zang), 0],
-                   [np.sin(zang), np.cos(zang), 0],
-                   [0,0,1]])
+#     # ZYX Euler angles
+#     t = glfw.get_time()
+#     xang = t
+#     yang = np.radians(30)
+#     zang = np.radians(30)
+#     M = np.identity(4)
+#     Rx = np.array([[1,0,0],
+#                    [0, np.cos(xang), -np.sin(xang)],
+#                    [0, np.sin(xang), np.cos(xang)]])
+#     Ry = np.array([[np.cos(yang), 0, np.sin(yang)],
+#                    [0,1,0],
+#                    [-np.sin(yang), 0, np.cos(yang)]])
+#     Rz = np.array([[np.cos(zang), -np.sin(zang), 0],
+#                    [np.sin(zang), np.cos(zang), 0],
+#                    [0,0,1]])
 
-    R = Rz @ Ry @ Rx
-    # # check inverse rotation
-    # R = Rz @ Ry @ Rx.T
+#     R = Rz @ Ry @ Rx
+#     # # check inverse rotation
+#     # R = Rz @ Ry @ Rx.T
 
-    # # check R @ R.T
-    # print(R @ R.T)
+#     # # check R @ R.T
+#     # print(R @ R.T)
 
-    # # check determinant
-    # print(np.linalg.det(R))
+#     # # check determinant
+#     # print(np.linalg.det(R))
 
-    M[:3,:3] = R
-    glMultMatrixf(M.T)
+#     M[:3,:3] = R
+#     glMultMatrixf(M.T)
 
-    glScalef(.25,.25,.25)
+#     glScalef(.25,.25,.25)
 
-    # draw cubes
-    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, (.5,.5,.5,1.))
-    drawCube_glDrawArray()
+#     # draw cubes
+#     glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, (.5,.5,.5,1.))
+#     drawCube_glDrawArray()
 
-    glTranslatef(2.5,0,0)
-    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, (1.,0.,0.,1.))
-    drawCube_glDrawArray()
+#     glTranslatef(2.5,0,0)
+#     glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, (1.,0.,0.,1.))
+#     drawCube_glDrawArray()
 
-    glTranslatef(-2.5,2.5,0)
-    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, (0.,1.,0.,1.))
-    drawCube_glDrawArray()
+#     glTranslatef(-2.5,2.5,0)
+#     glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, (0.,1.,0.,1.))
+#     drawCube_glDrawArray()
 
-    glTranslatef(0,-2.5,2.5)
-    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, (0.,0.,1.,1.))
-    drawCube_glDrawArray()
+#     glTranslatef(0,-2.5,2.5)
+#     glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, (0.,0.,1.,1.))
+#     drawCube_glDrawArray()
 
-    glDisable(GL_LIGHTING)
+#     glDisable(GL_LIGHTING)
